cd30d63181d4eb301c9c455559a81bec959dde5f
ruslan8424 <stoletniy12@gmail.com>
2024-11-08 08:33:03 +0000

    msql1

--- www/index.php
+++ www/index.php
@@ -1,1 +1,18 @@
-<?php phpinfo();
+<?php 
+
+$user = 'root';
+$pass = 'root';
+
+$dsn = 'mysql:host=mysql1;dbname=infarmation_schema;charset=utf8';
+$pdo = new PDO($dsn, $user, $pass);
+
+$stmt = $pdo->query('select * from tables');
+$row = $stmt->fetch();
+
+print_r($row)
+
+
+
+
+
+#phpinfo();
