a1aa4e9fdfc7cb953041c68da0fa4ddc7e306095
ruslan8424 <stoletniy12@gmail.com>
2024-11-09 04:11:25 +0000

    save

--- www/index.php
+++ www/index.php
@@ -3,13 +3,14 @@
 $user = 'root';
 $pass = 'root';
 
-$dsn = 'mysql:host=mysql1;dbname=infarmation_schema;charset=utf8';
+$dsn = 'mysql:host=mysql1;dbname=information_schema;charset=utf8';
 $pdo = new PDO($dsn, $user, $pass);
 
 $stmt = $pdo->query('select * from tables');
 $row = $stmt->fetch();
 
-print_r($row)
+print_r($row);
+
 
 
 
